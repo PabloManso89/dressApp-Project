b65ed28f1f674f182a88fcf8b936ce7d1101ce03
Alejandro Manso <alejandro.manso@cognizant.com>
2018-03-12 18:48:03 +0100

    replace deprecated module

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule } from '@angular/material';
 import { MdTabsModule } from 'md-tabs/tabs';
 
@@ -27,7 +27,7 @@ import { AppRoutingModule } from './routing/app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     MatInputModule,
     AppRoutingModule
   ],
