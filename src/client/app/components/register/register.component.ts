46c6179aafe34f16988abbde8ea8f4ad536421ea
pablomansogonzalez <Pablo.Manso.Gonzalez@sk.ibm.com>
2018-05-08 18:52:14 +0200

    Add register stepper

--- src/client/app/components/register/register.component.ts
+++ src/client/app/components/register/register.component.ts
@@ -7,20 +7,36 @@ import {FormBuilder, FormGroup, Validators, FormControl, Form} from '@angular/fo
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  selectedGender: string;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  emailFormControl: FormControl;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  ages = [
+    '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+    '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+    '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+    '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+    '50', '51', '52', '53', '54', '55', '56', '57', '58', '59',
+    '60', '61', '62', '63', '64', '65', '66', '67', '68', '69'];
+
+  genders = [
+    'Male',
+    'Female'
+  ];
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required, Validators.minLength(1)],
-      emailFormControl: ['', Validators.email]
+    this.firstFormGroup = this.fb.group({
+      nameControl: ['', Validators.required],
+      surnameControl: ['', Validators.required],
+      emailControl: ['', Validators.email]
     });
-    this.secondFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+    this.secondFormGroup = this.fb.group({
+      firstCtrl: ['', Validators.required],
     });
   }
+
+  // get name() { return this.firstFormGroup.get('firstCtrl'); }
+
 
 }
